62d2a23dc25bf9b2c5cc04508f10c32236fc1912
Eduardo Mora <eddiemora74@gmail.com>
2022-01-29 11:53:14 -0500

    added title

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
+
+import Head from 'next/head'; 
 import Body from '../components/Body';
 import Text from '../components/Text';
 import getWordle from '../server/getWordle';
 
 export default function Home({ number, word }) {
   return (
+    <>
+    <Head>
+      <title>Wordle of the day</title>
+    </Head>
     <Body>
       <Text>Wordle Number: {number}</Text>
       <Text>Wordle of the day: {word.toUpperCase()}</Text>
     </Body>
+    </>
+    
   )
 }
 
