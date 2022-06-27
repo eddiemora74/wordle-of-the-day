bd7ecbac63298a86d7099b67430330d6fc9f429d
Eduardo Mora <eddiemora74@gmail.com>
2022-06-27 18:31:52 -0400

    new routing mechanism for js file.

--- server/getWordle.js
+++ server/getWordle.js
@@ -11,12 +11,12 @@ export default async function getWordle() {
     const wordleText = await (await fetch(process.env.WORDLE_BASE_URL)).text();
 
     // Find the current hash of the mainjs file.
-    const hashFindStart = wordleText.indexOf(process.env.WORDLE_HASH_FIND_START) + process.env.WORDLE_HASH_FIND_START.length;
-    const hashFindEnd = wordleText.indexOf(process.env.WORDLE_HASH_FIND_END, hashFindStart);
-    const hash = wordleText.substring(hashFindStart, hashFindEnd);
+    const hashFindStart = wordleText.indexOf(process.env.WORDLE_HASH_FIND_START);
+    const hashFindEnd = wordleText.indexOf(process.env.WORDLE_HASH_FIND_END, hashFindStart) + process.env.WORDLE_HASH_FIND_END.length;
+    const hash = wordleText.substring(hashFindStart, hashFindEnd).replace(".css", ".js");
     
     // Make http request to get contents of main.js file.
-    const mainJsText = await (await fetch(`${process.env.WORDLE_BASE_URL}main.${hash}.js`)).text();
+    const mainJsText = await (await fetch(`${hash}`)).text();
 
     // Find the list of all possible values.
     const listFindStart = mainJsText.indexOf(process.env.WORDLE_LIST_FIND_START) - 1;
